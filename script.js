a50ab6827525fd51c2a9e3c3ca6db98963409116
marky2402 <marky2402@hotmail.com>
2025-09-04 14:57:30 +0100

    fixed bug - forgot to update linkWeekCommencing at line 34 in script.js to the correct link starting date of 31-08-25.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getCookie(cname) {
 
 // remember, 06/07/2023, is actually 06/08/2023 because Jan starts
 // at zero.
-const linkWeekCommencing = new Date(2025, 5, 1);
+const linkWeekCommencing = new Date(2025, 7, 31);
 let driv = getCookie('driver');
 let driver = driv ? driv : 'CAMBRIDGE';
 let currentWeekComm;
